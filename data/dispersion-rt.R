8b04ced1ec42f20a4650381f543b47ee5bc4ce43
George G. Vega Yon <g.vegayon@gmail.com>
2023-06-25 18:27:37 -0600

    Simulation study analyzing Rt

--- data/dispersion-rt.R
+++ data/dispersion-rt.R
@@ -1,19 +1,57 @@
 library(epiworldR)
+library(data.table)
 
-model <- ModelSIRCONN(
-  name = "A", n = 1e3, prevalence = 1/1e3, contact_rate = 2,
-  transmission_rate = .3, recovery_rate = 1/3
-  )
+r_rate   <- 1/3
+c_rate   <- 5 # Average observed
+nsims    <- 100
+nthreads <- 20
+n        <- 5e4
+
+rts <- c(.75, 1.5, 2, 2.5, 3)
+res <- NULL
 
 set.seed(1233)
-run_multiple(
-  model, 100, nsims = 500, saver = make_saver("reproductive"),
-  nthreads = 6
-  )
 
-plot(model)
+# Creating filenames
+fnames <- sprintf("%s/%%05lu-simulation-%s.csv", tempdir(), rts)
+fnames <- setNames(fnames, rts)
 
-repnums <- run_multiple_get_results(model)[[1]]
+for (rt in rts) {
 
-subset(repnums, source_exposure_date == 0)$rt |>
-  hist()
+  message("Running simulations for Rt = ", rt)
+
+  # Figuring out the transmission rate
+  t_rate <- rt * r_rate/(c_rate - rt + rt * r_rate) # Analytical sol
+
+  model <- ModelSIRCONN(
+    name = "A", n = n, prevalence = 1/n, contact_rate = c_rate,
+    transmission_rate = t_rate, recovery_rate = r_rate
+    )
+
+  run_multiple(
+    model, 100, nsims = nsims,
+    saver = make_saver("reproductive", fn = fnames[as.character(rt)]),
+    nthreads = nthreads
+    )
+
+  repnums <- data.table(run_multiple_get_results(model)[[1]])
+
+  # Making sure we clean all the files
+  unlink(list.files(tempdir(), full.names = TRUE, pattern = "simulation"))
+
+  repnums <- repnums[source_exposure_date == 0]
+
+  repnums <- c(repnums$rt, rep(0, nsims - nrow(repnums)))
+
+  res <- rbind(
+    res, data.table(
+    rt     = rt,
+    rt_obs = repnums,
+    t_rate = t_rate
+  ))
+
+  print(res[, mean(rt_obs), by = "rt"])
+
+}
+
+res[, mean(rt_obs), by = .(rt, t_rate)]
